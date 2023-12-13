4b0080904ea97cc820f7188e3d543405060d7440
Joseph Birr-Pixton <jpixton@gmail.com>
2023-12-13 17:57:13 +0000

    Pass through key exchange errors
    In 3355e06f9 we generalised the error type here, but we didn't
    get rid of code that discarded the information-less error.

--- rustls/src/client/hs.rs
+++ rustls/src/client/hs.rs
@@ -856,8 +856,7 @@ impl ExpectServerHelloOrHelloRetryRequest {
                     }
                 };
 
-                skxg.start()
-                    .map_err(|_| Error::FailedToGetRandomBytes)?
+                skxg.start()?
             }
             _ => offered_key_share,
         };
